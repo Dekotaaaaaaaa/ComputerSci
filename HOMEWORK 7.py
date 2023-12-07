f6f1a1f503ed19ace88f30cf776dab28cfea27bf
Dekotaaaaaaaa <153165569+Dekotaaaaaaaa@users.noreply.github.com>
2023-12-07 01:47:15 -0500

    Add files via upload

--- HOMEWORK 7.py
+++ HOMEWORK 7.py
@@ -2,14 +2,34 @@
 
 
 #Discussion Question 1 (pg. 278)
+#a The difference between these two are that definite loop it runs for a certian amount of times and indefinite loop runs until it is no longer true
+#b The while loop runs until a condition is false, and for loop is a definite loop that runs for however many times you want
+#c The difference is  interactive loops rely on user input, while sentinel loops use a predetermined condition to determine when to stop iterating.
+#d sentinel loop uses a designated value within the input data, while an End of file loop relies on the natural end-of-file indication in the input stream.
 
 
-#The difference between these the two are that a indefinite loop is implemented using a while statement and a definite loop is 
-
-#Discussion Question 2 (all sub-problems) (pg. 278/279)
 
 #Discussion Question 3 (a,c) (pg. 279)
+#a
 
+n = 10  sum_of_numbers = 0
+current_number = 1
+
+while current_number <= n:
+    sum_of_numbers += current_number
+    current_number += 1
+
+print("The sum of the first", n, "counting numbers is:", sum_of_numbers)
+
+#c
+sum_of_numbers = 0
+number = float(input("Enter a number (enter 999 to stop): "))
+
+while number != 999:
+    sum_of_numbers += number
+    number = float(input("Enter a number (enter 999 to stop): "))
+
+print("The sum of the entered numbers is:", sum_of_numbers)
 #Programming Exercise 1 (pg. 279)
 print( "P | Q| R | not (P and Q) | (not P) and Q | (not P) or (not Q)| (P and Q) or R | (P or R) and (Q or R)")
 for P in [True, False]:
